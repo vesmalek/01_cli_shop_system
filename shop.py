39f0d3fa49c552f66843e59b1db682ca50be7e8c
Ismail Malik <vesmalick@gmail.com>
2026-04-28 21:41:03 +0300

    Created the shop menu

---
+++ shop.py
@@ -0,0 +1,18 @@
+print()
+print("="*28)
+
+print("       ISMAIL'S SHOP")
+
+print("="*28)
+
+print("1. View Products")
+print("2. Add Item to Cart")
+print("3. View Cart")
+print("4. Remove Item from Cart")
+print("5. Apply Discount Code")
+print("6. Checkout")
+print("7. Exit")
+
+print("="*28)
+choice = input("Enter your choice: ")
+print()
